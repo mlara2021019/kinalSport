f11bf764b45052b119080d8452a795921bca074a
esantos2019254 <esantos-2019254@kinal.edu.gt>
2026-04-27 11:43:28 -0600

    feat: rutas protegidas

--- client-admin/src/app/router/AppRoutes.jsx
+++ client-admin/src/app/router/AppRoutes.jsx
@@ -1,17 +1,33 @@
 import { Routes, Route } from "react-router-dom";
 import { AuthPage } from "../../features/auth/pages/AuthPage.jsx";
 import { DashboardPage } from "../layouts/DashboardPage.jsx";
+import { RoleGuard } from "./RoleGuard.jsx";
+import { ProtectedRoute } from "./ProtectedRoute.jsx";
 import { Users } from "../../features/users/components/Users.jsx";
 import { Fields } from "../../features/fields/components/Fields.jsx";
 import { VerifyEmailPage } from "../../features/auth/pages/VerifyEmailPage.jsx";
+import { UnauthorizedPage } from "../../features/auth/pages/UnauthorizedPage.jsx";
 
 export const AppRoutes = () => {
     return (
         <Routes>
+            {/* RUTAS PUBLICAS */}
             <Route path="/" element={<AuthPage />} />
-            <Route path="/verify-email" element={<VerifyEmailPage />}/>
-            <Route path="/dashboard" element={<DashboardPage />} >
-                <Route path="users" element={<Users />}/>
+            <Route path="/verify-email" element={<VerifyEmailPage />} />
+            <Route path="unauthorized" element={<UnauthorizedPage />} />
+
+            {/* PROCTED ROUTES + ROLE */}
+            <Route
+                path="/dashboard"
+                element={
+                    <ProtectedRoute>
+                        <RoleGuard allowedRoles={["ADMIN_ROLE"]}>
+                            <DashboardPage />
+                        </RoleGuard>
+                    </ProtectedRoute>
+                }
+            >
+                <Route path="users" element={<Users />} />
                 <Route path="fields" element={<Fields />} />
             </Route>
         </Routes>
